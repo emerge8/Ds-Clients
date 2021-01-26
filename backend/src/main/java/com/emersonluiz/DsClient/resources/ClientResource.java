a479817bcc776eadbd6ed5ed24885f878ce42209
Emerson Luiz <emerson_egl@hotmail.com>
2021-01-26 16:43:40 -0300

    ClientResource

---
+++ backend/src/main/java/com/emersonluiz/DsClient/resources/ClientResource.java
@@ -0,0 +1,25 @@
+package com.emersonluiz.DsClient.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.emersonluiz.DsClient.entities.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Client>> findAll() {
+		List<Client> list = new ArrayList<>();
+		list.add(new Client (1L,"Lily"));
+		list.add(new Client(2L, "Diana"));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
