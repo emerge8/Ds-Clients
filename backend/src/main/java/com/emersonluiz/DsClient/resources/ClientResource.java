258a54a5907164b6bb50c7043fcf54664f4c8110
Emerson Luiz <emerson_egl@hotmail.com>
2021-01-28 15:05:37 -0300

    Pagination

--- backend/src/main/java/com/emersonluiz/DsClient/resources/ClientResource.java
+++ backend/src/main/java/com/emersonluiz/DsClient/resources/ClientResource.java
@@ -2,9 +2,11 @@ package com.emersonluiz.DsClient.resources;
 
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,8 +32,18 @@ public class ClientResource {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<ClientDTO>> findAll() {
-		List<ClientDTO> list = service.findAll();
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy)
+			
+			 {
+		
+		PageRequest  pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 			return ResponseEntity.ok().body(list);
 	}
 	
@@ -51,7 +64,7 @@ public class ClientResource {
 		
 		@PutMapping(value = "/{id}")
 		public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {	
-		dto = service.update(id,dto);
+		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 		}
 		
